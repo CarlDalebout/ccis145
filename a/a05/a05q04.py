eed303ce3b059bee66e37c3f8da4396fb2854aa0
CarlDalebout <cadalebout1@couger.ciss.edu>
2023-10-12 16:25:36 -0500

    finished a04

--- a/a05/a05q04.py
+++ a/a05/a05q04.py
@@ -1,9 +1,11 @@
+import random
 def encrypt_n(n = 1):
     string = input("Please give me a sting to encrypt: ")
     new_string = ""
     string_list = string.split()
-    encrypted_string_list = ''
+    encrypted_string_list = []
     for i in string_list:
+        new_string = ""
         for j in i:
             if(ord(j) > ord('a') and ord(j) < ord('z') + n): #( 32 > 96 and 32 < 122)
                 if(ord(j) + n > ord('z')):
@@ -11,28 +13,54 @@ def encrypt_n(n = 1):
                 else:
                     new_char = chr((ord(j) + n))
                 new_string += new_char
-        encrypted_string_list += new_string
-        print(new_string)
+        encrypted_string_list += [new_string]
+    print(str(encrypted_string_list))
+    return encrypted_string_list
 
-def decrypt_n(n = 1):
-    string = input("please give me a string to decrypt: ")
-    new_string = ""
-    string_list = string.split()
-    decrypted_string_list = ""
+def decrypt_shift(string_list, shift_amount):
+    string_list = string_list.split()
+    decrypted_string_list = []
     for i in string_list:
-        for j in i:
-                if(ord(j)> ord('a') - n and ord(j) < ord('z')):
-                    if(ord(j) - n) < ord('a'):
-                        new_char = chr((ord(j)) - n + 26)
-                    else:
-                        new_char = chr((ord(j)) - n)
+        decrypted_string = ""
+        for char in i:
+            if(ord(char) > ord('a') - shift_amount  and ord(char) < ord('z')):
+                if(ord(char) - shift_amount < ord('a')):
+                    decrypted_char = chr((ord(char) - shift_amount) + 26)
+                else:
+                    decrypted_char = chr(ord(char) - shift_amount)
+                decrypted_string += decrypted_char
+        decrypted_string_list += [decrypted_string]
+    return decrypted_string_list
 
-                    new_string += new_char
-        decrypted_string_list += new_string
-        print(new_string)
+def check_decryption(decrypted_string_list):
+    
+    file = open("wordlist.txt", "r")
+    words = file.read().splitlines()
+    found = True
+    for decrypted_word in decrypted_string_list:
+        for element in words:
+            if(element != decrypted_word):
+                found = False
+    return found
+
+def find_shift():
+    found = False
+    string = input("Enter your encrypted text: ")    
+    for shift_amount in range(2, 25):
+        if(check_decryption(decrypt_shift(string, shift_amount))):
+            found = True   
+            break 
+    if(found):
+        print("\nDecrypted text: ", end= '')
+        for elements in decrypt_shift(string, shift_amount):
+            print(elements, end= ' ')
+        print("\nMethod used for decryption: ", shift_amount, "-character shift substituition", sep = '')
+    else:
+        print("\nDecryption failed")    
+
 
 def main():
-    encrypt_n(2)
-    decrypt_n(2)
+    encrypt_n(random.randint(1, 25))
+    find_shift()
 
 main()
